22bd29b26ccb2d81737a8b84340641c08b3bc733
Clara Miralles <clara.miralles.imperial@gmail.com>
2019-07-07 22:53:24 +0200

    implemented image for menu button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const menuButtonElement = document.querySelector('.nav-trigger');
+const menuImageElement = document.createElement(img);
+menuImageElement.setAttribute ('src', '../images/ico-menu.svg');
+menuButtonElement.appendChild(menuImageElement);
 const navElement = document.querySelector('.nav');
 navElement.classList.add('hidden');
 const closeButtonElement = document.querySelector('.nav-close');
