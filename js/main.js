9cdbc7634277bf00dcebdaaf25aa0bf90fa20cd4
Clara Miralles <clara.miralles.imperial@gmail.com>
2019-07-07 23:28:39 +0200

    implemented feature to close menu with button and working ok in correlation with showing the menu

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
 'use strict';
 
 const menuButtonElement = document.querySelector('.nav-trigger');
-
 const menuImageElement = document.createElement('img');
-
 menuImageElement.setAttribute ('src', '../images/ico-menu.svg');
 menuButtonElement.appendChild(menuImageElement);
 
 const navElement = document.querySelector('.nav');
 navElement.classList.add('hidden');
-const closeButtonElement = document.querySelector('.nav-close');
 
-function handlerShowMenu () {
-navElement.classList.contains('hidden') ? navElement.classList.remove('hidden') :navElement.classList.add('hidden');
+const closeButtonElement = document.querySelector('.nav-close');
+const closeImageElement = document.createElement('img');
+closeImageElement.setAttribute('src', '../images/ico-close.svg');
+closeButtonElement.appendChild(closeImageElement);
+
+function handlerMenu() {
+    showMenu()
+    hideMenuButton()
+    hideCloseButton()
 };
 
-menuButtonElement.addEventListener('click', handlerShowMenu);
+function showMenu() {
+    navElement.classList.contains('hidden') ? navElement.classList.remove('hidden') :navElement.classList.add('hidden');
+};
+
+function hideMenuButton(){
+    navElement.classList.contains('hidden') ? menuButtonElement.classList.remove('hidden') :menuButtonElement.classList.add ('hidden');
+};
+
+function handlerCloseButton(){
+    showMenu()
+    hideCloseButton()
+    hideMenuButton()
+}
+function hideCloseButton(){
+    navElement.classList.contains('hidden')? closeButtonElement.classList.add('hidden') : closeButtonElement.classList.remove('hidden');
+}
+
+menuButtonElement.addEventListener('click', handlerMenu);
+closeButtonElement.addEventListener('click', handlerCloseButton);
